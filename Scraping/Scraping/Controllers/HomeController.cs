ef3444679fdf3af0266d35c1c504d223d27b2937
swornimnepal2000@gmail.com <swornim@12345>
2017-03-28 21:34:56 +0545

    scrapping app

---
+++ Scraping/Scraping/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+using Scraping.Models;
+
+namespace Scraping.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            HtmlWeb hWeb = new HtmlWeb();
+            HtmlDocument doc = hWeb.Load("http://www.espncricinfo.com/ci/engine/match/index.html?view=live");
+            HtmlModel ht = new HtmlModel();
+
+            HtmlModel lstHtml = new HtmlModel();
+            var header1 = doc.DocumentNode.SelectNodes("//section[@id='live-match-data']").ToList();
+            var header = doc.DocumentNode.SelectSingleNode("//section[@id='live-match-data']");
+            // var header = doc.DocumentNode.InnerHtml;
+            //doc.DocumentNode.SelectNodes("//section[@id='live-match-data']").ToString();
+            ht.Html = header.ToString();
+          /*  lstHtml.Add(ht)*/;
+            lstHtml.Html = header.InnerHtml.ToString();
+            foreach (var item in header1) {
+                Console.WriteLine(item.InnerHtml);
+            }
+            return View(lstHtml);
+            
+        }
+    }
+}
---
+++ Scraping/Scraping/Models/HtmlModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scraping.Models
+{
+    public class HtmlModel
+    {
+        public string Html { get; set; }
+    }
+}
